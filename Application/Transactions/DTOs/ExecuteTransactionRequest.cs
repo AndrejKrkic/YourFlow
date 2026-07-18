7a428e733e2d9e9da531230c0a93b8aeaeaaee03
AndrejKrkic <andrejkrkic46@gmail.com>
2026-07-18 18:39:48 +0200

    Add transaction DTOs

---
+++ Application/Transactions/DTOs/ExecuteTransactionRequest.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Enums;
+
+namespace Application.Transactions.DTOs
+{
+    public sealed record ExecuteTransactionRequest(
+    Guid AccountId,
+    Guid CategoryId,
+    decimal Amount,
+    TransactionType Type,
+    DateTime TransactionDate,
+    string? Description);
+}
---
+++ Application/Transactions/DTOs/ExecuteTransactionResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Transactions.DTOs
+{
+    public sealed record ExecuteTransactionResult
+    {
+        Guid TransactionID  { get; set; }
+        decimal NewAccountBalance { get; set; }
+    }
+}
